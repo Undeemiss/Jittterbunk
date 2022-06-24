d54a622834773c63083f5ae03e01934694d011c2
Jade Hewitt <jadehewitt@Jades-Air.attlocal.net>
2022-06-24 11:26:13 -0500

    Created User, Bunk models and migrated

--- bunk/models.py
+++ bunk/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    # Limit username length
+    username = models.CharField(max_length=20)
+    # Allow long image urls
+    photo = models.CharField(max_length=256)
+
+
+class Bunk(models.Model):
+    # Allow bunks sent by deleted users to remain
+    from_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='bunks_sent')
+    # Delete bunks given to deleted users
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bunks_received')
+    # Automatically set bunk times for convenience
+    time = models.DateTimeField(auto_now_add = True)
