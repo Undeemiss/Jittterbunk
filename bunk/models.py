440936965ae2c4fcde89986769938c89bec145d4
Jade Hewitt <jadehewitt@Jades-Air.attlocal.net>
2022-06-24 11:56:30 -0500

    Add str methods to models; create test bunk

--- bunk/models.py
+++ bunk/models.py
@@ -4,7 +4,10 @@ class User(models.Model):
     # Limit username length
     username = models.CharField(max_length=20)
     # Allow long image urls
-    photo = models.CharField(max_length=256)
+    photo = models.CharField(max_length=256, null=True)
+
+    def __str__(self):
+        return self.username;
 
 
 class Bunk(models.Model):
@@ -14,3 +17,6 @@ class Bunk(models.Model):
     to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bunks_received')
     # Automatically set bunk times for convenience
     time = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return '"' + str(self.from_user) + '" bunked "' + str(self.to_user) + '"';
