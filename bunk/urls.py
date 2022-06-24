fc18ed055c1813bdab94d2ac8ddd385dd33d419b
Jade Hewitt <jadehewitt@Jades-Air.attlocal.net>
2022-06-24 16:53:25 -0500

    Added user feed with bunk button

--- bunk/urls.py
+++ bunk/urls.py
@@ -6,4 +6,6 @@ app_name = 'bunk'
 urlpatterns = [
     path('', views.redirect_global_feed),
     path('feed', views.GlobalFeed.as_view(), name='global_feed'),
+    path('<int:user_id>/feed/', views.user_feed, name='user_feed'),
+    path('<int:to_user_id>/bunk/', views.bunk_submit, name='bunk_submit'),
 ]
--- bunk/views.py
+++ bunk/views.py
@@ -1,15 +1,40 @@
 from django.views import generic
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import get_object_or_404, render
 
-from .models import Bunk
+from .models import Bunk, User
 
 def redirect_global_feed(request):
     return HttpResponseRedirect(reverse('bunk:global_feed'))
 
+# List of all bunks
 class GlobalFeed(generic.ListView):
     template_name = 'bunk/global_feed.html'
     context_object_name = 'bunk_list'
 
     def get_queryset(self):
-        return Bunk.objects.order_by('time')
+        return Bunk.objects.order_by('-time')
+
+# List of a user's received bunks
+def user_feed(request, user_id, error=None):
+    user = get_object_or_404(User, pk=user_id)
+    bunk_list = user.bunks_received.order_by('-time');
+    return render(request, 'bunk/user_feed.html', {'user': user, 'bunk_list': bunk_list, 'error': error})
+
+# Handle Bunk submission
+def bunk_submit(request, to_user_id):
+    # Check if the submitted username is valid
+    from_user_name = request.POST['from_username']
+    try:
+        from_user = User.objects.get(username = from_user_name)
+    except (KeyError, User.DoesNotExist):
+        err_msg = f'No user found with username "{from_user_name}"'
+        return user_feed(request, to_user_id, err_msg)
+    
+    # Create the bunk if the username is valid
+    to_user = get_object_or_404(User, pk=to_user_id)
+    new_bunk = Bunk(to_user=to_user, from_user=from_user)
+    new_bunk.save()
+
+    return HttpResponseRedirect(reverse('bunk:user_feed', args=(to_user.id,)))
